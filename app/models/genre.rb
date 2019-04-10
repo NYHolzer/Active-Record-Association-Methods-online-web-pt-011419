fe456935ed61a736fd5ea7799aa6ec0b69d4a1b3
Nissan Holzer <nissan.holzer@gmail.com>
2019-04-10 00:47:21 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,5 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     art_name = []
+    self.artists.each {|a| art_name << a.name}
+    art_name
   end
 end
